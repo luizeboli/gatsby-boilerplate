c086acf0133bcd557baedf2a56fdd7dd0c892ce7
luizeboli <unifelicio@gmail.com>
2020-08-17 12:21:38 -0300

    feat: add global styles to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from '@styles/globalStyles';
+
 import { Container } from './styles';
 
 const Layout: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+  return (
+    <>
+      <GlobalStyle />
+      <Container>{children}</Container>
+    </>
+  );
 };
 
 export default Layout;
